dc3c084fc0bdae79872b42ddf07951920f8c04a7
aramind <r.m.miranda.ece@gmail.com>
2023-07-09 00:25:12 +0800

    implementing the header section

--- modern-ui-ux-website/src/containers/header/Header.js
+++ modern-ui-ux-website/src/containers/header/Header.js
@@ -1,8 +1,46 @@
 import React from "react";
 import "./header.css";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div
+      className="gpt3__header section__padding"
+      id="home"
+    >
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Lets Build Something amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis,
+          optio similique delectus obcaecati eveniet repellat ipsam nobis a quo
+          saepe.
+        </p>
+        <div className="gpt3__header-content__input">
+          <input
+            type="email"
+            placeholder="Your email address"
+          />
+          <button type="button">Get Started</button>
+        </div>
+        <div className="gpt3__header-content__people">
+          <img
+            src={people}
+            alt="people"
+          />
+          <p>1,600 people requested access</p>
+        </div>
+      </div>
+      <div className="gpt3__header-image">
+        <img
+          src={ai}
+          alt="AI"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
